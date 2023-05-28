06f381a8fefd69afedbc47a6cb855810275e33e3
Drikus Roor <drikusroor@gmail.com>
2023-05-28 21:06:45 +0200

    Return better message if there are no posts

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -17,7 +17,7 @@ export const QUERY = gql`
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div>Empty</div>
+export const Empty = () => <div>There are no posts</div>
 
 export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
