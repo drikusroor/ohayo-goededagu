fe3bf066da226601987cb2b02065668b0d649dd9
Drikus Roor <drikusroor@gmail.com>
2023-06-12 21:42:14 +0200

    Add logout for logged in user

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -7,21 +7,29 @@ type BlogLayoutProps = {
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
-  const { isAuthenticated, currentUser } = useAuth()
+  const { isAuthenticated, logOut, currentUser } = useAuth()
   return (
     <>
       <header className="w-full pb-5 text-center">
-        {isAuthenticated && (
+        {isAuthenticated && currentUser && (
           <div className="flex items-center justify-between bg-red-500 p-3 text-white">
             <span>Logged in as {currentUser.email}</span>
-            <ul>
+            <ul className="flex items-center gap-3">
               <li>
                 <Link
-                  className="rounded bg-gray-900 px-4 py-2 font-bold text-white hover:bg-gray-700"
+                  className="block rounded bg-blue-900 px-4 py-2 font-bold text-white transition-colors hover:bg-blue-800"
                   to={routes.admin()}
                 >
                   Admin
                 </Link>
+              </li>
+              <li>
+                <button
+                  className="rounded bg-red-900 px-4 py-2 font-bold text-white transition-colors hover:bg-red-800"
+                  onClick={logOut}
+                >
+                  Logout
+                </button>
               </li>
             </ul>
           </div>
