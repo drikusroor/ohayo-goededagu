3b0349ca4cdfb62aab150a82b9dd4e987b1ef25f
Naomi Eliasar <naomi@omnimap.nl>
2023-06-14 12:13:32 +0200

    feature: Logout button matches styling of the other buttons

--- web/src/layouts/AdminDashboardLayout/AdminDashboardLayout.tsx
+++ web/src/layouts/AdminDashboardLayout/AdminDashboardLayout.tsx
@@ -33,7 +33,7 @@ const AdminDashboardLayout = ({ children }: AdminDashboardLayoutProps) => {
             <button
               type="button"
               onClick={logOut}
-              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              className="rw-button rw-button-red"
             >
               Logout
             </button>
