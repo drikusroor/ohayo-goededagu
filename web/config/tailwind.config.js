97eb2e241d626a8c635b95f42a6c867fc3a41c02
Drikus Roor <drikusroor@gmail.com>
2023-06-17 16:30:37 +0200

    feat: Add logo colors to tailwind configuration

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cobalt-blue': '#1e4785',
+        'monza-red': '#BC002D',
+      },
+    },
   },
   plugins: [],
 }
