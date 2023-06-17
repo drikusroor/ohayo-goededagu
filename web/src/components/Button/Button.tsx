38c51ba3780e005f16e94d818d899d43c54a671a
Drikus Roor <drikusroor@gmail.com>
2023-06-17 16:59:16 +0200

    feat: Add button type to Button component

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
   color?: 'cobalt-blue' | 'monza-red' | ''
   children?: React.ReactNode
   text?: string
+  type?: 'button' | 'submit' | 'reset'
 }
 
 const Button = ({
@@ -12,9 +13,12 @@ const Button = ({
   color = '',
   text = '',
   children,
+  type = 'button',
 }: ButtonProps) => {
   return (
     <button
+      type={type}
+      aria-label={text}
       className={`block rounded bg-cobalt-blue px-3 py-2 text-xs font-semibold uppercase text-white transition hover:brightness-110 hover:filter
       ${color ? `bg-${color}` : ''}
       ${className}
