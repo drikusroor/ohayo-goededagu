1be8c836f93e070d01b3ed973e743a6d79c339ee
Drikus Roor <drikusroor@gmail.com>
2023-06-27 13:31:59 +0200

    chore: Some cleanup

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -2,14 +2,7 @@ import { MetaTags } from '@redwoodjs/web'
 
 import { Role } from 'src/types/role'
 
-import Adriana from '../../../../web/public/images/Adriana.jpg'
-import Drikus from '../../../../web/public/images/Drikus.jpg'
-import Emiel from '../../../../web/public/images/Emiel.jpg'
-import Naomi from '../../../../web/public/images/Naomi.jpg'
-import Robert from '../../../../web/public/images/Robert.jpg'
 import ReisgenootschapCell from '../../components/ReisgenootschapCell'
-
-import Person from './Person'
 
 const reisgenootschapRoles = [Role.ADMIN, Role.MODERATOR]
 
