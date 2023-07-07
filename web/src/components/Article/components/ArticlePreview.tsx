987aa4e580d5bb104bf2000c719669f40863a183
Naomi Eliasar <naomi@omnimap.nl>
2023-07-07 21:43:48 +0200

    Create avatar timestamp component and expand articleVideo component

--- web/src/components/Article/components/ArticlePreview.tsx
+++ web/src/components/Article/components/ArticlePreview.tsx
@@ -36,12 +36,17 @@ const ArticlePreview = ({ article }: Props) => {
     >
       {article.type === EPostType.ARTICLE && (
         <section className="rounded bg-gray-600 bg-[url('https://cdn.cheapoguides.com/wp-content/uploads/sites/2/2020/05/akihabara-iStock-484915982-1024x683.jpg')] bg-cover bg-center bg-no-repeat bg-blend-multiply">
-          <div className="mx-auto max-w-screen-xl px-4 py-24 text-center lg:py-56">
-            <h1 className="mb-4 flex items-center justify-center gap-2 text-3xl font-extrabold leading-none tracking-tight text-white md:gap-4 md:text-5xl lg:text-6xl">
-              <ArticleTypeIcon type={article.type as EPostType} />
-              {article.title}
-            </h1>
-            <p className="mb-8 line-clamp-3 text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
+          <div className="mx-auto max-w-screen-xl px-4 py-20 md:py-24 lg:py-56">
+            <div className="flex flex-row items-center justify-center gap-2 pb-2">
+              <div className="">
+                <ArticleTypeIcon type={article.type as EPostType} />
+              </div>
+
+              <h1 className="flex flex-wrap items-center justify-center text-3xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+                {article.title}
+              </h1>
+            </div>
+            <p className="mb-8 line-clamp-3 text-center text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
               {article.body}
             </p>
             <div className="flex flex-row items-center justify-center gap-12">
@@ -97,7 +102,9 @@ const ArticlePreview = ({ article }: Props) => {
         </section>
       )}
 
-      {article.type != EPostType.ARTICLE && (
+      {article.type === EPostType.VIDEO && <ArticleVideo article={article} />}
+
+      {article.type != EPostType.ARTICLE && article.type != EPostType.VIDEO && (
         <>
           <header className="mb-3">
             <div className="mt-4 flex flex-row items-center gap-2 pl-1">
@@ -114,18 +121,6 @@ const ArticlePreview = ({ article }: Props) => {
           </header>
           <div className="lg:mx-14">
             <div>
-              {article.type === EPostType.VIDEO && (
-                <ArticleVideo videoPost={article.videoPost} />
-              )}
-
-              {article.type === EPostType.ARTICLE && (
-                <img
-                  alt="cover"
-                  className="aspect-video w-full object-cover"
-                  src="https://cdn.cheapoguides.com/wp-content/uploads/sites/2/2020/05/akihabara-iStock-484915982-1024x683.jpg"
-                />
-              )}
-
               {(article.type === EPostType.HAIKU ||
                 article.type === EPostType.CHOTTO) && (
                 <div className="justmt-2">{article.body}</div>
--- web/src/components/Article/components/ArticleVideo.tsx
+++ web/src/components/Article/components/ArticleVideo.tsx
@@ -1,34 +1,68 @@
 import { useMemo } from 'react'
 
+import { BsArrowRightCircle } from 'react-icons/bs'
+import { Post } from 'types/graphql'
+
+import { Link, navigate, routes } from '@redwoodjs/router'
+
+import ArticleTypeIcon, {
+  EPostType,
+} from 'src/components/ArticleTypeIcon/ArticleTypeIcon'
+import AvatarTimestamp from 'src/components/Avatar/AvatarTimestamp/AvatarTimestamp'
+import Button from 'src/components/Button/Button'
+
 import extractVideoID from './helpers/extract-video-id'
 
 interface Props {
-  videoPost: {
-    videoUrl: string
-  }
+  article: Post
 }
 
-const ArticleVideo = ({ videoPost }: Props) => {
+const ArticleVideo = ({ article }: Props) => {
   const embedUrl = useMemo(() => {
-    const videoId = extractVideoID(videoPost?.videoUrl)
+    const videoId = extractVideoID(article?.videoPost?.videoUrl)
     if (!videoId) return null
     return `https://www.youtube.com/embed/${videoId}`
-  }, [videoPost?.videoUrl])
+  }, [article?.videoPost?.videoUrl])
 
   if (!embedUrl) return null
 
   return (
-    <div className="aspect-video bg-slate-300">
-      <iframe
-        src={embedUrl}
-        className="h-full w-full"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
-    </div>
+    <>
+      <header className="mb-3">
+        <div className="mt-4 flex flex-row items-center gap-2 pl-1">
+          <ArticleTypeIcon type={EPostType.VIDEO} />
+          <h2
+            className="text-xl font-semibold text-slate-700 md:text-2xl"
+            title={article.title}
+          >
+            <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+          </h2>
+        </div>
+      </header>
+      <div className="aspect-video bg-slate-300">
+        <iframe
+          src={embedUrl}
+          className="h-full w-full"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div className="flex flex-row items-center justify-between pt-4">
+        <AvatarTimestamp article={article} />
+        {article.type === EPostType.VIDEO && article.body && (
+          <Button
+            className="flex items-center gap-2 px-4 py-3 text-xs"
+            onClick={() => navigate(routes.article({ id: article.id }))}
+          >
+            <span className="hidden sm:inline-block">Lees verder</span>
+            <BsArrowRightCircle />
+          </Button>
+        )}
+      </div>
+    </>
   )
 }
 
