bb05cdf94312d099abefdf22155ae7678ba81fb0
Drikus Roor <drikusroor@gmail.com>
2023-07-31 10:38:58 +0200

    feat: Add separate video component purely for showing videos

---
+++ web/src/components/Video/Video.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react'
+
+import extractVideoID from '../Article/components/helpers/extract-video-id'
+
+const Video = ({ embedUrl = '' }) => {
+  const formattedEmbedUrl = useMemo(() => {
+    if (!embedUrl) return null
+
+    const videoId = extractVideoID(embedUrl)
+    if (!videoId) return null
+    return `https://www.youtube.com/embed/${videoId}`
+  }, [embedUrl])
+
+  return (
+    <div className="aspect-video bg-slate-300">
+      <iframe
+        src={formattedEmbedUrl}
+        className="h-full w-full"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        loading="lazy"
+      ></iframe>
+    </div>
+  )
+}
+
+export default Video
