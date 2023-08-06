97f9a31fd42dabff5f6937e056d700b77b204c1a
Naomi Eliasar <nreliasar@gmail.com>
2023-08-06 12:59:01 +0200

    fix double video in this branch

--- web/src/components/Article/components/ArticleVideo.tsx
+++ web/src/components/Article/components/ArticleVideo.tsx
@@ -1,36 +1,32 @@
-import { useMemo } from 'react'
-
 import { BsArrowRightCircle } from 'react-icons/bs'
 import { Post } from 'types/graphql'
 
 import { Link, navigate, routes } from '@redwoodjs/router'
 
+import ArticleCommentCountBadge from 'src/components/ArticleCommentCountBadge/ArticleCommentCountBadge'
 import ArticleTypeIcon, {
   EPostType,
 } from 'src/components/ArticleTypeIcon/ArticleTypeIcon'
 import AvatarTimestamp from 'src/components/Avatar/AvatarTimestamp/AvatarTimestamp'
 import Button from 'src/components/Button/Button'
-
-import extractVideoID from './helpers/extract-video-id'
+import Video from 'src/components/Video/Video'
+import { EPostDisplayType } from 'src/types/post-display-type.enum'
 
 interface Props {
   article: Post
-  type: EPostType
+  displayType: EPostDisplayType
   date?: string
 }
 
-const ArticleVideo = ({ article, type, date }: Props) => {
-  const embedUrl = useMemo(() => {
-    const videoId = extractVideoID(article?.videoPost?.videoUrl)
-    if (!videoId) return null
-    return `https://www.youtube.com/embed/${videoId}`
-  }, [article?.videoPost?.videoUrl])
+const ArticleVideo = ({ article, displayType, date }: Props) => {
+  const authorName =
+    article.user?.profile?.name || article.user?.name || 'Anonymous'
 
-  if (!embedUrl) return null
+  if (!article?.videoPost?.videoUrl) return null
 
   return (
     <>
-      {type === EPostType.PREVIEW && (
+      {displayType === EPostDisplayType.PREVIEW && (
         <>
           <header className="mb-3">
             <div className="mt-4 flex flex-row items-center gap-2 pl-1">
@@ -45,33 +41,33 @@ const ArticleVideo = ({ article, type, date }: Props) => {
               </h2>
             </div>
           </header>
-          <div className="aspect-video bg-slate-300">
-            <iframe
-              src={embedUrl}
-              className="h-full w-full"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-              loading="lazy"
-            ></iframe>
-          </div>
+
+          <Video embedUrl={article?.videoPost?.videoUrl} />
+
           <div className="flex flex-row items-center justify-between pt-4">
             <AvatarTimestamp article={article} />
-            {article.body && (
-              <Button
-                className="flex items-center gap-2 px-4 py-3 text-xs"
-                onClick={() => navigate(routes.article({ id: article.id }))}
-              >
-                <span className="hidden sm:inline-block">Lees verder</span>
-                <BsArrowRightCircle />
-              </Button>
-            )}
+            <div className="flex items-center gap-6">
+              {article.comments.length > 0 && (
+                <ArticleCommentCountBadge
+                  count={article.comments.length}
+                  variant="dark"
+                />
+              )}
+              {article.body && (
+                <Button
+                  className="flex items-center gap-2 px-4 py-3 text-xs"
+                  onClick={() => navigate(routes.article({ id: article.id }))}
+                >
+                  <span className="hidden sm:inline-block">Lees verder</span>
+                  <BsArrowRightCircle />
+                </Button>
+              )}
+            </div>
           </div>
         </>
       )}
 
-      {type === EPostType.FULL && (
+      {displayType === EPostDisplayType.FULL && (
         <>
           <header className="flex flex-col gap-1">
             <h1 className="flex items-center gap-2 text-3xl font-extrabold uppercase tracking-tight md:gap-4">
@@ -80,27 +76,11 @@ const ArticleVideo = ({ article, type, date }: Props) => {
             </h1>
 
             <div className="flex flex-row items-center gap-2">
-              <span className="text-sm text-slate-500">
-                {article.user.name
-                  ? article.user.name
-                  : article.user.email
-                  ? article.user.email
-                  : 'Anonymous'}
-              </span>
+              <span className="text-sm text-slate-500">{authorName}</span>
               <span className="text-sm text-slate-500"> | {date}</span>
             </div>
           </header>
-          <div className="aspect-video bg-slate-300">
-            <iframe
-              src={embedUrl}
-              className="h-full w-full"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-              loading="lazy"
-            ></iframe>
-          </div>
+          <Video embedUrl={article?.videoPost?.videoUrl} />
           <div> {article.body} </div>
         </>
       )}
