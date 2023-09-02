355c5033505085acd660d0edbfc76ae6881a006b
Drikus Roor <drikusroor@gmail.com>
2023-09-02 19:20:41 +0200

    fix: Fix markdown editor tests by supplying correct props

--- web/src/components/MarkdownEditor/MarkdownEditor.test.tsx
+++ web/src/components/MarkdownEditor/MarkdownEditor.test.tsx
@@ -8,7 +8,15 @@ import MarkdownEditor from './MarkdownEditor'
 describe('MarkdownEditor', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<MarkdownEditor />)
+
+      const name = 'test'
+      const value = 'test'
+      const onChange = () => {}
+      const placeholder = 'test'
+      const className = 'test'
+      const validation = { required: true}
+
+      render(<MarkdownEditor name={name} value={value} onChange={onChange} placeholder={placeholder} className={className} validation={validation} />)
     }).not.toThrow()
   })
 })
