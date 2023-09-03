8fa24d1f5b87a0cfefbbef236ca40eb692e64b9e
Drikus Roor <drikusroor@gmail.com>
2023-09-03 17:01:55 +0200

    db(seed): Add posts to seeder

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -69,6 +69,34 @@ export default async () => {
         },
       })
     }
+
+    const posts = [
+      {
+        title: 'First Post',
+        body: 'This is my first post',
+        published: true,
+        userId: 1,
+      },
+      {
+        title: 'Second Post',
+        body: `# Exuit corpora\n ## Utraque deae quo pars gestamina subterque de Lorem markdownum saucius te veniam portas arsit tactas. \n\nNon mediis. ipx_newline -= null; if (rate_printer(integrated.exploitVariable(dma + emulation, -1, -2), 554416 * file)) { ripcording = -1; peripheral_e_task = 5; } var maskMarketClock = code_computer_cpl - software_linux_cpu( reimageTouchscreenTerabyte(stringCron)); \n\nSilvis tu satis mentem. Tertius nos etiam sede deieci cuspis habebat disparibus labare inque quicquam! [Dabat certius](http://cornuaultime.net/monstratum-amento.php) mihi. \n\n ## Securi lacrimaeque nupta at ab nascentia imperat Fessis exilio esse videre, nuda Solem quoque pectus est capitis [luctus](http://www.lecti.com/), unius omni. Et caruit ista bracchia urbis nuper.\n\n - Nostra deploratosque gemitu desit\n - Equinis sanguis saepe primum`,
+        published: true,
+        userId: 1,
+      },
+      {
+        title: 'Third Post',
+        body: '# A chotto matte\n\n ## Utraque deae quo pars gestamina subterque de\nLorem markdownum saucius te veniam portas arsit tactas. Non mediis.\n- Nostra deploratosque gemitu desit\n- Equinis sanguis saepe primum',
+        published: true,
+        userId: 1,
+        type: 'CHOTTO',
+      },
+    ]
+
+    for (const post of posts) {
+      await db.post.create({
+        data: post,
+      })
+    }
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
