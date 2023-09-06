f613e8c856b77b058c16515f1ee2406a4243f2eb
Drikus Roor <drikusroor@gmail.com>
2023-09-06 21:37:17 +0200

    fix: Fix posts / my posts distinction in routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -29,10 +29,13 @@ const Routes = () => {
         <Set wrap={AdminDashboardLayout}>
           <Route path="/admin" page={() => <Redirect to={adminRedirect} />} name="admin" />
           <Set wrap={ScaffoldLayout} title="My Posts" titleTo="myPosts" buttonLabel="New Post" buttonTo="newPost">
+            <Route path="/admin/my-posts" page={PostMyPostsPage} name="myPosts" />
+          </Set>
+          <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
             <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
             <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
             <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
-            <Route path="/admin/my-posts" page={PostMyPostsPage} name="myPosts" />
+            <Route path="/admin/posts" page={PostPostsPage} name="posts" />
           </Set>
           <Set wrap={ScaffoldLayout} title="Profile" titleTo="profileSelf" buttonLabel="New Profile" buttonTo="newProfile">
             <Route path="/admin/profile/new" page={ProfileNewProfilePage} name="newProfile" />
