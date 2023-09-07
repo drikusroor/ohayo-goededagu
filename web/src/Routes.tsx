c7dcbb9706f806f590c9608b48302e6e46de2e2c
Drikus Roor <drikusroor@gmail.com>
2023-09-07 12:28:07 +0200

    Fixed: Fix dashboard (#112)
    * fix: Fix posts / my posts distinction in routes
    
    * fix: Fix minor typescript issues
    
    * fix: Fix more minor typescript issues

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,14 +14,15 @@ import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import { useAuth } from './auth'
 import AdminDashboardLayout from './layouts/AdminDashboardLayout/AdminDashboardLayout'
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
+import { Role } from './types/role'
 
 const BlogLoader = () => <BlogLayout skeleton />
 
 const Routes = () => {
   const { currentUser } = useAuth()
-
   const requiredRolesAdminPosts = ['ADMIN', 'MODERATOR']
-  const adminRedirect = currentUser?.roles.some((role) => requiredRolesAdminPosts.includes(role)) ? '/admin/posts' : '/admin/profile/self'
+  const roles = (currentUser?.roles || []) as Role[]
+  const adminRedirect = roles.some((role) => requiredRolesAdminPosts.includes(role)) ? '/admin/posts' : '/admin/profile/self'
 
   return (
     <Router useAuth={useAuth}>
@@ -29,10 +30,13 @@ const Routes = () => {
         <Set wrap={AdminDashboardLayout}>
           <Route path="/admin" page={() => <Redirect to={adminRedirect} />} name="admin" />
           <Set wrap={ScaffoldLayout} title="My Posts" titleTo="myPosts" buttonLabel="New Post" buttonTo="newPost">
+            <Route path="/admin/my-posts" page={PostMyPostsPage} name="myPosts" />
+          </Set>
+          <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
             <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
             <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
             <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
-            <Route path="/admin/my-posts" page={PostMyPostsPage} name="myPosts" />
+            <Route path="/admin/posts" page={PostPostsPage} name="posts" />
           </Set>
           <Set wrap={ScaffoldLayout} title="Profile" titleTo="profileSelf" buttonLabel="New Profile" buttonTo="newProfile">
             <Route path="/admin/profile/new" page={ProfileNewProfilePage} name="newProfile" />
