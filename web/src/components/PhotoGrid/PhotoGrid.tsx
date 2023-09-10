6bc2bdab0b0843434c26a74660294eba690a2856
Drikus Roor <drikusroor@gmail.com>
2023-09-10 16:49:20 +0200

    fix: Fix className issue in photogrid

--- web/src/components/PhotoGrid/PhotoGrid.tsx
+++ web/src/components/PhotoGrid/PhotoGrid.tsx
@@ -1,34 +1,20 @@
 import { Image } from 'types/graphql'
 
-interface Props {
+import { classNames } from 'src/lib/class-names'
+
+interface IPhotoGridProps extends React.HTMLAttributes<HTMLDivElement> {
   images: Image[]
   preview?: boolean
 }
 
-const PhotoGrid = ({ images = [], preview }: Props) => {
+const PhotoGrid = ({ className, images = [], preview }: IPhotoGridProps) => {
   const previewGallery = images?.slice(0, 5)
 
   return (
-    <>
-      <div className="mt-4 bg-gray-200 p-2">
-        <ul className="flex flex-wrap gap-4">
-          {preview &&
-            previewGallery.map((photo) => {
-              return (
-                <li
-                  className="relative h-[300px] grow basis-auto last:flex-initial"
-                  key={photo.imageId}
-                >
-                  <img
-                    className="h-full w-full rounded-md object-cover align-middle"
-                    key={photo.imageId}
-                    src={photo.url}
-                    alt={photo.imageId}
-                  />
-                </li>
-              )
-            })}
-          {images.map((photo) => {
+    <div className={classNames('mt-4 bg-gray-200 p-2', className)}>
+      <ul className="flex flex-wrap gap-4">
+        {preview &&
+          previewGallery.map((photo) => {
             return (
               <li
                 className="relative h-[300px] grow basis-auto last:flex-initial"
@@ -43,9 +29,23 @@ const PhotoGrid = ({ images = [], preview }: Props) => {
               </li>
             )
           })}
-        </ul>
-      </div>
-    </>
+        {images.map((photo) => {
+          return (
+            <li
+              className="relative h-[300px] grow basis-auto last:flex-initial"
+              key={photo.imageId}
+            >
+              <img
+                className="h-full w-full rounded-md object-cover align-middle"
+                key={photo.imageId}
+                src={photo.url}
+                alt={photo.imageId}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
