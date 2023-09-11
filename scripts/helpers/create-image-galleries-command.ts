eb6b082ef5ab461a7c52dac75c7fc12f8fdb0ded
Drikus Roor <drikusroor@gmail.com>
2023-09-11 15:39:01 +0200

    fix: Fix photo grid preview display

--- scripts/helpers/create-image-galleries-command.ts
+++ scripts/helpers/create-image-galleries-command.ts
@@ -2,10 +2,14 @@ export default function getImageGalleriesCreateCommand({
   galleriesAmount = 1,
   imagesAmount = 6,
 }) {
-  const images = Array.from({ length: imagesAmount }, (_, i) => ({
-    imageId: `b1b9a0c0-9f0a-11eb-8dcd-0242ac13000${i}`,
-    url: `https://picsum.photos/seed/${i}/200/300`,
-  }))
+  const images = Array.from({ length: imagesAmount }, (_, i) => {
+    const randomWidth = Math.floor(Math.random() * 200) + 100
+    const randomHeight = Math.floor(Math.random() * 200) + 100
+    return {
+      imageId: `b1b9a0c0-9f0a-11eb-8dcd-0242ac13000${i}`,
+      url: `https://picsum.photos/seed/${i}/${randomWidth}/${randomHeight}`,
+    }
+  })
 
   const galleries = Array.from({ length: galleriesAmount }, (_, i) => ({
     imageGallery: {
