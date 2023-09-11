2e9449c7b94077ed03c5331e041e809906bca7c2
Drikus Roor <drikusroor@gmail.com>
2023-09-11 11:21:12 +0200

    feat: Improve blog layout skeletons (#125)

--- web/src/layouts/BlogLayout/BlogLayout.stories.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.stories.tsx
@@ -6,6 +6,10 @@ export const generated: ComponentStory<typeof BlogLayout> = (args) => {
   return <BlogLayout {...args} />
 }
 
+export const loading: ComponentStory<typeof BlogLayout> = (args) => {
+  return <BlogLayout {...args} skeleton />
+}
+
 export default {
   title: 'Layouts/BlogLayout',
   component: BlogLayout,
