f90cb8cf08ca118bb2e77affbc80434874797658
Drikus Roor <drikusroor@gmail.com>
2023-09-16 16:29:12 +0200

    feat: Improve email template for forgot password

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -23,12 +23,47 @@ export const handler = async (
     // address in a toast message so the user will know it worked and where
     // to look for the email.
     handler: async (user) => {
+      const userName = user.profile?.name
+        ? user.profile.name
+        : user.name
+        ? user.name
+        : 'bezoeker'
+
       // Send email to user with password reset link
       sendEmail({
         to: user.email,
         subject: 'Password Reset',
-        html: `<a href="https://ohayo-goededagu.nl/reset-password?resetToken=${user.resetToken}">Reset Password</a>`,
-        text: `Reset Password: https://ohayo-goededagu.nl/reset-password?resetToken=${user.resetToken}`,
+        html: `<p>Beste ${userName},</p><p>Je hebt een verzoek gedaan om je wachtwoord te resetten.</p><p>Klik op de link om je wachtwoord te resetten: <a href="https://ohayo-goededagu.nl/reset-password?resetToken=${user.resetToken}">Reset Password</a></p>
+
+        <p>Als je geen verzoek hebt gedaan om je wachtwoord te resetten, dan kun je deze email negeren.</p>
+
+        <p>Mocht je nog vragen hebben, dan kun je altijd contact met ons opnemen.</p>
+
+        <p>Met vriendelijke groet,</p>
+
+        <p>Ohayo Goededagu</p>
+
+        <p><a href="https://ohayo-goededagu.nl" target="_blank" rel="noopener noreferrer">https://ohayo-goededagu.nl</a></p>
+
+        <p><a href="mailto:info@ohayo-goededagu.nl" target="_blank" rel="noopener noreferrer">info@ohayo-goededagu.nl</a></p>`,
+        text: `Beste ${userName},
+
+Je hebt een verzoek gedaan om je wachtwoord te resetten.
+
+Klik op de link om je wachtwoord te resetten: https://ohayo-goededagu.nl/reset-password?resetToken=${user.resetToken}
+
+Als je geen verzoek hebt gedaan om je wachtwoord te resetten, dan kun je deze email negeren.
+
+Mocht je nog vragen hebben, dan kun je altijd contact met ons opnemen.
+
+Met vriendelijke groet,
+
+Ohayo Goededagu
+
+https://ohayo-goededagu.nl
+
+info@ohayo-goededagu.nl
+`,
       })
 
       await db.userAction.create({
