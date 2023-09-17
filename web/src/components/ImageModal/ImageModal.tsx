7ea910f454d353e14f7b64976cbafacf6bdd6368
Naomi Eliasar <nreliasar@gmail.com>
2023-09-17 20:51:38 +0200

    replace div with button

--- web/src/components/ImageModal/ImageModal.tsx
+++ web/src/components/ImageModal/ImageModal.tsx
@@ -1,3 +1,8 @@
+import { BsXLg } from 'react-icons/bs'
+import { Image } from 'types/graphql'
+
+import Button from '../Button/Button'
+
 interface Props {
   info: object
 }
@@ -12,17 +17,22 @@ const ImageModal = ({ info }: Props) => {
       id="modal"
       className="fixed left-0 top-0 z-10 hidden h-full w-full overflow-auto bg-black bg-opacity-70 pt-2.5 text-white"
     >
-      <div
-        className="absolute right-6 top-20 cursor-pointer text-4xl font-bold text-white"
+      <Button
+        className="absolute right-6 top-20 cursor-pointer bg-transparent text-4xl font-bold text-white hover:bg-transparent hover:text-slate-300"
         onClick={() => {
           closeModal()
         }}
       >
-        &times;
-      </div>
+        <BsXLg />
+      </Button>
       <div
         className="flex h-full w-full items-center justify-center px-20 py-20"
+        role="button"
+        tabIndex={0}
         onClick={() => {
+          closeModal()
+        }}
+        onKeyDown={() => {
           closeModal()
         }}
       >
@@ -32,7 +42,7 @@ const ImageModal = ({ info }: Props) => {
             className="h-full w-fit rounded-md"
             src={info?.url}
             key={info?.id}
-            alt={info?.id}
+            alt={info?.title}
           />
           {info?.title && <span className="text-xl">{info?.title}</span>}
           {info?.description && <span>{info?.description}</span>}
