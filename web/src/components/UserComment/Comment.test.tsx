08196d5d2568c368e95b61916ad0ad410cf344cf
Drikus Roor <drikusroor@gmail.com>
2023-09-19 11:15:36 +0200

    feat: Add reply button to comment component

--- web/src/components/UserComment/Comment.test.tsx
+++ web/src/components/UserComment/Comment.test.tsx
@@ -1,60 +1,79 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, act } from '@redwoodjs/testing/web'
 
 import Comment from './Comment'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
 
+const user = {
+  id: 1,
+  name: 'test',
+  email: 'info@example.com',
+  posts: [],
+  roles: [],
+}
+
+const post = {
+  id: 1,
+  title: 'test',
+  body: 'test',
+  createdAt: '2021-02-22T20:02:20.000Z',
+  comments: [],
+  published: true,
+  type: 'ARTICLE',
+  updatedAt: '2021-02-22T20:02:20.000Z',
+  user,
+  imageGalleries: [],
+}
+
+const comment = {
+  id: 1,
+  body: 'test',
+  createdAt: '2021-02-22T20:02:20.000Z',
+  thumbs: [],
+  deleted: false,
+  postId: 1,
+  post,
+  userId: 1,
+  user,
+  children: [
+    {
+      id: 2,
+      body: 'ik ben een kindje',
+      createdAt: '2021-02-22T20:02:20.000Z',
+      thumbs: [],
+      deleted: false,
+      postId: 1,
+      post,
+      userId: 1,
+      user,
+      children: [],
+    },
+  ],
+}
+
 describe('Comment', () => {
   it('renders successfully', () => {
     expect(() => {
-      const user = {
-        id: 1,
-        name: 'test',
-        email: 'info@example.com',
-        posts: [],
-        roles: [],
-      }
-
-      const post = {
-        id: 1,
-        title: 'test',
-        body: 'test',
-        createdAt: '2021-02-22T20:02:20.000Z',
-        comments: [],
-        published: true,
-        type: 'ARTICLE',
-        updatedAt: '2021-02-22T20:02:20.000Z',
-        user,
-      }
-
-      const comment = {
-        id: 1,
-        body: 'test',
-        createdAt: '2021-02-22T20:02:20.000Z',
-        thumbs: [],
-        deleted: false,
-        postId: 1,
-        post,
-        userId: 1,
-        user,
-        children: [
-          {
-            id: 2,
-            body: 'ik ben een kindje',
-            createdAt: '2021-02-22T20:02:20.000Z',
-            thumbs: [],
-            deleted: false,
-            postId: 1,
-            post,
-            userId: 1,
-            user,
-            children: [],
-          },
-        ],
-      }
-
       render(<Comment comment={comment} />)
     }).not.toThrow()
+  })
+
+  it('calls onClickReply when reply button is clicked', async () => {
+    const onClickReply = jest.fn()
+
+    const { getByTestId } = render(
+      <Comment comment={comment} onClickReply={onClickReply} />
+    )
+
+    const replyButton = getByTestId('replyButton')
+
+    await act(async () => {
+      replyButton.click()
+    })
+
+    expect(onClickReply).toHaveBeenCalled()
+
+    expect(onClickReply).toHaveBeenCalledWith(comment)
   })
 })
