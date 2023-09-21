0a6e4220715c6114aa236e182a14093153ae3a3a
Drikus Roor <drikusroor@gmail.com>
2023-09-21 17:16:38 +0200

    revert: Revert to sending emails using Brevo / Sendinblue

--- api/src/lib/email.ts
+++ api/src/lib/email.ts
@@ -11,20 +11,17 @@ export async function sendEmail({ to, subject, text, html }: Options) {
   console.log('Sending email to:', to)
 
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
+    host: 'smtp-relay.brevo.com',
     secure: true,
-    port: 465,
+    port: 587,
     auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false,
+      user: 'drikusroor@icloud.com',
+      pass: process.env.SEND_IN_BLUE_KEY,
     },
   })
 
   const info = await transporter.sendMail({
-    from: '"Ohayo Noreplyo" <noreply@ohayo-goededagu.nl>',
+    from: '"Drikus from Ohayo Noreplyo" <drikusroor@icloud.com>',
     to: Array.isArray(to) ? to : [to], // list of receivers
     subject,
     text,
