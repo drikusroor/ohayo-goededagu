0a7efe9a5964c8d375822e73c73d27eaba8702b9
Drikus Roor <drikusroor@gmail.com>
2023-09-22 13:13:39 +0200

    fix: Correctly parse post types & authors

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -9,8 +9,10 @@ const HomePage = () => {
   const queryInput = {
     page: params.page ? parseInt(params.page) : 1,
     perPage: params.perPage ? parseInt(params.perPage) : 10,
-    postTypes: params.postTypes ? params.postTypes : [],
-    authors: params.authors ? params.authors : [],
+    postTypes: params.postTypes ? params.postTypes.split(',') : [],
+    authors: params.authors
+      ? params.authors.split(',').map((a) => parseInt(a))
+      : [],
   }
 
   return (
