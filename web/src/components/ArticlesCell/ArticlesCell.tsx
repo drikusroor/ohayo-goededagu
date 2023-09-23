ff25657827259fe7699587ae87ca56eb693a0397
Drikus Roor <drikusroor@gmail.com>
2023-09-23 17:26:37 +0200

    refactor: Simplify links to vlog/gallery pages

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -82,39 +82,7 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({ result, vlog, gallery }: Props) => {
-  const vlogs = result.posts.filter((article) => {
-    if (article.type === EPostType.VIDEO) {
-      return article
-    }
-  })
-
-  const galleries = result.posts.filter((article) => {
-    if (article.type === EPostType.PHOTO_GALLERY) {
-      return article
-    }
-  })
-
-  if (vlog) {
-    return (
-      <ul className="flex flex-col justify-center gap-6 p-3 md:gap-12 md:p-10">
-        {vlogs.map((article) => {
-          return <ArticlePreview key={article.id} article={article} />
-        })}
-      </ul>
-    )
-  }
-
-  if (gallery) {
-    return (
-      <ul className="flex flex-col justify-center gap-6 p-3 md:gap-12 md:p-10">
-        {galleries.map((article) => {
-          return <ArticlePreview key={article.id} article={article} />
-        })}
-      </ul>
-    )
-  }
-
+export const Success = ({ result }: Props) => {
   return (
     <ul className="flex flex-col justify-center gap-6 p-3 md:gap-12 md:p-10">
       {result.posts.map((article) => {
