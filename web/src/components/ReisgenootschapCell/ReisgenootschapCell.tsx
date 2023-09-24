1937fb15c9844ce54f487a9064ced216ffb5c3cc
Naomi Eliasar <nreliasar@gmail.com>
2023-09-24 10:54:17 +0200

    skeleton for reisgezelschap #188

--- web/src/components/ReisgenootschapCell/ReisgenootschapCell.tsx
+++ web/src/components/ReisgenootschapCell/ReisgenootschapCell.tsx
@@ -6,6 +6,7 @@ import type {
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import Reisgenootschap from '../Reisgenootschap/Reisgenootschap'
+import Skeleton from '../Skeleton/Skeleton'
 
 export const QUERY = gql`
   query FindReisgenootschapQuery($roles: [Role!]!) {
@@ -22,7 +23,15 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => (
+  <div className="grid max-w-6xl md:grid md:grid-cols-2">
+    {Array.from({ length: 5 }).map((_, i) => (
+      <div key={i}>
+        <Skeleton className="m-3 h-28 rounded-md md:m-10 md:h-52 " />
+      </div>
+    ))}
+  </div>
+)
 
 export const Empty = () => <div>Empty</div>
 
