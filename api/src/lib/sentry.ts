a50f4e05b7d0110126442d02ecffe72dbf8a2997
Drikus Roor <drikusroor@gmail.com>
2023-09-24 11:56:10 +0200

    chore: Decrease sample rate

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -5,7 +5,7 @@ Sentry.init({
   dsn: 'https://0a31bfc51a00df59a4436abeeb6915f4@o4505913207160832.ingest.sentry.io/4505913211748352',
   integrations: [new ProfilingIntegration()],
   // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  tracesSampleRate: 0.2, // Capture 100% of the transactions, reduce in production!
   // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  profilesSampleRate: 0.2, // Capture 100% of the transactions, reduce in production!
 })
