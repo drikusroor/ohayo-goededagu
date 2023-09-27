c3f5138df6a9a0a63f4fc52e3ffdd26ee7f369dd
Naomi Eliasar <nreliasar@gmail.com>
2023-09-27 21:12:34 +0200

    responsive map #229

--- web/src/pages/RoutePage/RoutePage.tsx
+++ web/src/pages/RoutePage/RoutePage.tsx
@@ -4,12 +4,15 @@ const RoutePage = () => {
   return (
     <>
       <MetaTags title="Route" description="Route page" />
-      <iframe
-        title="Route"
-        src="https://www.google.com/maps/d/embed?mid=1ArsHjITlNvUl5SemqV6B5QmF6xmgqk8&ehbc=2E312F&noprof=1"
-        width="640"
-        height="480"
-      ></iframe>
+      <div className="h-[20rem] w-[20rem] p-4 sm:w-[35rem] md:h-5/6 md:w-[45rem] lg:w-[60rem]">
+        <iframe
+          title="Route"
+          src="https://www.google.com/maps/d/embed?mid=1ArsHjITlNvUl5SemqV6B5QmF6xmgqk8&ehbc=2E312F&noprof=1"
+          // width="640"
+          // height="480"
+          className="h-full w-full"
+        ></iframe>
+      </div>
     </>
   )
 }
